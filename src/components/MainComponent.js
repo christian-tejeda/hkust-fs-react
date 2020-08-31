ef4b75e0ece093729afb1366d8ae474192d6c9c9
Christian <christian.tejeda@fing.edu.uy>
2020-08-31 05:45:06 -0300

    React Redux forms pt. 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,10 +9,10 @@ import FooterComponent from './FooterComponent';
 import HomeComponent from './HomeComponent';
 import AboutComponent from './AboutComponent';
 import ContactComponent from './ContactComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+// import { DISHES } from '../shared/dishes';
+// import { COMMENTS } from '../shared/comments';
+// import { PROMOTIONS } from '../shared/promotions';
+// import { LEADERS } from '../shared/leaders';
 
 const mapStateToProps = state => {
     return {
